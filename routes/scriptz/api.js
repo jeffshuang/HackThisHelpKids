2fdbb746b734335d5feed167753dba6d15961120
jhsu2 <jhsu2@andrew.cmu.edu>
2018-10-06 03:30:16 -0400

    translated to javascript

--- routes/scriptz/api.js
+++ routes/scriptz/api.js
@@ -1,6 +1,6 @@
 function detectImage(inputUrl) {
 	var subscriptionKey = "0001beb2e84743f88ebb524de2421c20";
-	var uriBase = "https://eastus.api.cognitive.microsoft.com/face/v1.0"
+	var uriBase = "https://eastus.api.cognitive.microsoft.com/face/v1.0/detect"
 
 	var params = {
             "returnFaceId": "true",
@@ -26,6 +26,58 @@ function detectImage(inputUrl) {
 
     .done(function(data) {
             // Show formatted JSON on webpage.
-            $("#responseTextArea").val(JSON.stringify(data, null, 2));
+            //$("#responseTextArea").val(JSON.stringify(data, null, 2));
+            console.log(JSON.stringify(data,null,2));
     })
+
+    .fail(function(jqXHR, textStatus, errorThrown) {
+            // Display error message.
+            var errorString = (errorThrown === "") ?
+                "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+            errorString += (jqXHR.responseText === "") ?
+                "" : (jQuery.parseJSON(jqXHR.responseText).message) ?
+                    jQuery.parseJSON(jqXHR.responseText).message :
+                        jQuery.parseJSON(jqXHR.responseText).error.message;
+            alert(errorString);
+    });
+
+}
+
+function matchFace(keyImage, inputImage) {
+	var subscriptionKey = "0001beb2e84743f88ebb524de2421c20";
+	var uriBase = "https://eastus.api.cognitive.microsoft.com/face/v1.0/verify"
+
+	var params = {};
+
+    $.ajax({
+        url: uriBase + "?" + $.param(params),
+
+        // Request headers.
+        beforeSend: function(xhrObj){
+            xhrObj.setRequestHeader("Content-Type","application/json");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+        },
+
+        type: "POST",
+
+        // Request body.
+        data: '{"faceId1": ' +'"'+ keyImage + '"' + ', "faceId2": ' +'"'+ inputImage +'"'+ '}',
+    })
+
+    .done(function(data) {
+            // Show formatted JSON on webpage.
+            //$("#responseTextArea").val(JSON.stringify(data, null, 2));
+            console.log(JSON.stringify(data,null,2));
+    })
+
+    .fail(function(jqXHR, textStatus, errorThrown) {
+            // Display error message.
+            var errorString = (errorThrown === "") ?
+                "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+            errorString += (jqXHR.responseText === "") ?
+                "" : (jQuery.parseJSON(jqXHR.responseText).message) ?
+                    jQuery.parseJSON(jqXHR.responseText).message :
+                        jQuery.parseJSON(jqXHR.responseText).error.message;
+            alert(errorString);
+    });
 }
