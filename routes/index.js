e0f06ae535e0a84dce507fe5ddcc3bdfff6acf80
Andy Gao <agao@andrew.cmu.edu>
2018-10-06 04:23:41 -0400

    t

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var firebase = require('firebase');
+var firebase = require("firebase");
 var router = express.Router();
 
 /* GET home page. */
@@ -7,23 +7,23 @@ router.get("/", function(req, res, next) {
   // addKidImage();
   loginUser();
   addNewKid();
-  res.render("index", { title: "Penis" });
+  res.render("index");
 });
 
 var config = {
-    apiKey: "AIzaSyAUdsE2TisvisIIX6hWEyTkQ0xWvJERTUY",
-    authDomain: "hackthekids-ac0c9.firebaseapp.com",
-    databaseURL: "https://hackthekids-ac0c9.firebaseio.com",
-    projectId: "hackthekids-ac0c9",
-    storageBucket: "hackthekids-ac0c9.appspot.com",
-    messagingSenderId: "187488860542"
-  };
+  apiKey: "AIzaSyAUdsE2TisvisIIX6hWEyTkQ0xWvJERTUY",
+  authDomain: "hackthekids-ac0c9.firebaseapp.com",
+  databaseURL: "https://hackthekids-ac0c9.firebaseio.com",
+  projectId: "hackthekids-ac0c9",
+  storageBucket: "hackthekids-ac0c9.appspot.com",
+  messagingSenderId: "187488860542"
+};
 firebase.initializeApp(config);
-
 
 function makeId() {
   var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   for (var i = 0; i < 10; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -32,21 +32,22 @@ function makeId() {
 }
 
 // Get a reference to the database service
-var database = firebase.database().ref('/');
+var database = firebase.database().ref("/");
 
 function addNewKid() {
   console.log("Adding New Kid");
-  var childRef = firebase.database().ref('child/' + 'mtQa2' + '/key');
+  var childRef = firebase.database().ref("child/" + "mtQa2" + "/key");
 
   childRef.set("pornhubkey.com");
-
 }
-
 
 function addKidImage(id) {
   console.log("Adding Kid");
   var newId = makeId();
-  var childRef = firebase.database().ref('child/' + newId + '/images').push();
+  var childRef = firebase
+    .database()
+    .ref("child/" + newId + "/images")
+    .push();
 
   var date = new Date();
   var time = date.valueOf();
@@ -54,16 +55,14 @@ function addKidImage(id) {
   var obj = {
     date: time,
     url: "www.pornhub.com"
-  }
+  };
 
   childRef.set(obj);
 }
 
-
 function loginUser() {
-  database.on('value', function(snapshot) {
-    database.once('value', function(snapshot) {
-
+  database.on("value", function(snapshot) {
+    database.once("value", function(snapshot) {
       var foundChild;
       var foundParent;
 
@@ -78,16 +77,14 @@ function loginUser() {
         }
         console.log(childData);
       });
-
     });
   });
 }
 
-
 function processChildren(data) {
   console.log("processChildren");
 
-  var found = false
+  var found = false;
 
   //Loop through kids
   Object.keys(data).forEach(function(key) {
@@ -97,7 +94,6 @@ function processChildren(data) {
     console.log(newObj["key"]);
     var url = newObj["key"];
     var found = found || checkImage(url);
-
   });
 
   return found;
@@ -106,6 +102,5 @@ function processChildren(data) {
 function checkImage(url) {
   return false;
 }
-
 
 module.exports = router;
