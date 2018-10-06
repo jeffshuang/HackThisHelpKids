3f3dd040b1abee1a798d68172caf96a7ae658649
lijeffrey39 <lijeffrey39@gmail.com>
2018-10-06 01:08:17 -0400

    firebase

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
+  // addKidImage();
+  loginUser();
+  addNewKid();
   res.render("index", { title: "Penis" });
 });
 
@@ -16,6 +19,93 @@ var config = {
     messagingSenderId: "187488860542"
   };
 firebase.initializeApp(config);
+
+
+function makeId() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 10; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+// Get a reference to the database service
+var database = firebase.database().ref('/');
+
+function addNewKid() {
+  console.log("Adding New Kid");
+  var childRef = firebase.database().ref('child/' + 'mtQa2' + '/key');
+
+  childRef.set("pornhubkey.com");
+
+}
+
+
+function addKidImage(id) {
+  console.log("Adding Kid");
+  var newId = makeId();
+  var childRef = firebase.database().ref('child/' + newId + '/images').push();
+
+  var date = new Date();
+  var time = date.valueOf();
+
+  var obj = {
+    date: time,
+    url: "www.pornhub.com"
+  }
+
+  childRef.set(obj);
+}
+
+
+function loginUser() {
+  database.on('value', function(snapshot) {
+    database.once('value', function(snapshot) {
+
+      var foundChild;
+      var foundParent;
+
+      snapshot.forEach(function(childSnapshot) {
+        var childKey = childSnapshot.key;
+        var childData = childSnapshot.val();
+
+        if (childKey == "child") {
+          foundChild = processChildren(childData);
+        } else {
+          foundParent = processChildren(childData);
+        }
+        console.log(childData);
+      });
+
+    });
+  });
+}
+
+
+function processChildren(data) {
+  console.log("processChildren");
+
+  var found = false
+
+  //Loop through kids
+  Object.keys(data).forEach(function(key) {
+    var newObj = data[key];
+
+    //Verify key url
+    console.log(newObj["key"]);
+    var url = newObj["key"];
+    var found = found || checkImage(url);
+
+  });
+
+  return found;
+}
+
+function checkImage(url) {
+  return false;
+}
 
 
 module.exports = router;
